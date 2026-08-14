b93493ffbe978d2cb0d1bd567056aab270e9c3eb
Wa <17301692273@163.com>
2026-08-14 20:48:56 +0800

    day2

---
+++ chat_gui.py
@@ -0,0 +1,100 @@
+import tkinter as tk
+root = tk.Tk()
+root.title("Python 聊天室")
+root.geometry("700x500")
+title_label = tk.Label(
+    root,
+    text="Python 聊天室",
+    font=("Arial", 18)
+)
+title_label.pack(pady=10)
+main_frame = tk.Frame(root)
+main_frame.pack(
+    fill="both",
+    expand=True,
+    padx=10,
+    pady=10
+)
+chat_frame = tk.Frame(
+    main_frame,
+    bd=1,
+    relief="solid"
+)
+chat_frame.pack(
+    side="left",
+    fill="both",
+    expand=True,
+    padx=(0, 10)
+)
+scrollbar = tk.Scrollbar(chat_frame)
+
+scrollbar.pack(
+    side="right",
+    fill="y"
+)
+chat_text = tk.Text(
+    chat_frame,
+    state="disabled",
+    yscrollcommand=scrollbar.set
+)
+chat_text.pack(
+    side="left",
+    fill="both",
+    expand=True
+)
+scrollbar.config(
+    command=chat_text.yview
+)
+user_frame = tk.Frame(
+    main_frame,
+    width=150,
+    bd=1,
+    relief="solid"
+)
+user_frame.pack(
+    side="right",
+    fill="y"
+)
+
+user_label = tk.Label(
+    user_frame,
+    text="在线用户",
+    font=("Arial", 12)
+)
+user_label.pack(pady=10)
+user_list = tk.Listbox(
+    user_frame,
+    width=15
+)
+user_list.pack(
+    fill="both",
+    expand=True,
+    padx=10,
+    pady=(0, 10)
+)
+user_list.insert("end", "杨过")
+user_list.insert("end", "令狐冲")
+user_list.insert("end", "宇文逸")
+input_frame = tk.Frame(root)
+input_frame.pack(
+    fill="x",
+    padx=10,
+    pady=(0, 10)
+)
+message_entry = tk.Entry(
+    input_frame
+)
+message_entry.pack(
+    side="left",
+    fill="x",
+    expand=True,
+    padx=(0, 10)
+)
+send_button = tk.Button(
+    input_frame,
+    text="发送"
+)
+send_button.pack(
+    side="right"
+)
+root.mainloop()
